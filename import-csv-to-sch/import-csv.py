a432ef6078785e3faad7b8169ec87d75fcba50ac
Oren Yagev <Oren Yagev>
2016-11-26 03:16:03 +0200

    Works for a single sheet

--- import-csv-to-sch/import-csv.py
+++ import-csv-to-sch/import-csv.py
@@ -3,6 +3,7 @@ import pprint
 
 import sys
 from sch.sch import Schematic
+from sch.editor import ComponentEditor
 import argparse
 
 
@@ -11,10 +12,20 @@ parser.add_argument('--csv', dest='csv_file', required=True,
                     help='CSV file as input')
 parser.add_argument('--sch', dest='sch_file', required=True,
                     help='Schematic file as output')
-parser.add_argument('--col-ref', dest='column_reference', required=False, default="Ref",
+parser.add_argument('--col-ref', dest='column_reference', required=False, default="Reference",
                     help='Reference column name')
 
 args = parser.parse_args()
+
+sch = Schematic(args.sch_file)
+sch_components = sch.components
+map_components = {}
+for c in sch_components:
+    for f in c.fields:
+        if int(f['id']) == 0:
+            map_components[f['ref'].strip('"')] = c
+            continue
+
 
 csvf = open(args.csv_file, 'r')
 reader = csv.reader(csvf)
@@ -24,8 +35,6 @@ components = {}
 
 
 fields = reader.next()
-#fields = map(str.strip, fields)
-
 
 if args.column_reference in fields:
     refIndex = fields.index(args.column_reference)
@@ -39,16 +48,16 @@ else:
 
 for row in reader:
     ref = row[0].strip()
-    if ref in components.keys():
-        sys.stderr.write('Component with reference "%s" already exist on previous row\n' % (ref))
-        sys.exit()
+    try:
+        component = map_components[ref]
+        ed = ComponentEditor(component)
+        for i in range(len(row))[1:]:
+            if row[i] != '':
+                ed.set_field_value(fields[i].strip(), row[i].strip())
 
-    components[ref] = {}
+    except KeyError:
+        print 'Can\'t find component %s \n' % ref
 
-    for i in range(len(row))[1:]:
-        if row[i] != '':
-            components[ref][fields[i].strip()] = row[i].strip()
-
-pprint.pprint(components)
+sch.save()
 
 
